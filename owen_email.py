6cc1add1ee01c499667a8ad92d26f840ce71693d
whmrtm <whmrtm@gmail.com>
2016-09-16 14:02:21 -0400

    email shortcut

--- owen_email.py
+++ owen_email.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python
 
+import smtplib
+from email.mime.text import MIMEText
+
+
+
 user = "whmrtm@gmail.com"
 passwd = "whm9316rtm"
 
@@ -13,6 +18,25 @@ class email:
                 self._content = ""
         def send(self):
                 self.get_info()
+                body = MIMEText(self._content, 'html')
+                body['From'] = self._from
+                body['To'] = self._to
+                body['Subject'] = self._subject
+                server = smtplib.SMTP("smtp.gmail.com", 587)
+                server.ehlo()
+                server.starttls()
+                try:
+                        server.login(self._from, self._passwd)
+                        print("Login successful")
+                except:
+                        print("Login error!")
+
+                try:
+                        server.sendmail(self._from, self._to, body.as_string())
+                        print("Mail sent!")
+                except:
+                        print("Mail sent error!")
+                server.close()
 
         def get_info(self):
                 print("Please input the email address you want to send to: ")
