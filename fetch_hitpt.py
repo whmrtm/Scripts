fd9a5cd79501ae9b41dcecbc4968a58b0341348e
whmrtm <whmrtm@gmail.com>
2016-09-21 14:14:31 -0400

    update

--- fetch_hitpt.py
+++ fetch_hitpt.py
@@ -4,30 +4,23 @@ import requests
 import re
 import urllib.request
 import sys
-
+import json
 from my_email import email
 
 
-my_url = "https://pt.hit.edu.cn/login.php"
-payload = {
-          "username" : "whmrtn@126.com",
-          "password" : "whm9316rtm"
-                }
+my_url = "https://pt.hit.edu.cn/login.php?returnto=torrents.php"
 
 session_requests = requests.session()
 # result = session_requests.get(my_url)
-result = session_requests.post(
-                my_url,
-                data = payload,
-                headers = dict(referer = my_url)
+payload = json.dumps({'username':'whmrtn@126.com', 'password':'whm9316rtm'})
+result = session_requests.post(my_url, data = payload, headers = dict(referer = my_url)
                 )
 print(result.status_code)
+print(result.history)
 print(result.text)
 
-
-
 torrent_url = "https://pt.hit.edu.cn/torrents.php"
-torrent_html = session_requests.get(torrent_url, headers = dict(referer = torrent_url))
+torrent_response = session_requests.get(torrent_url, headers = dict(referer = torrent_url), allow_redirects = True)
 
 
 # Send the email through my email class
@@ -36,4 +29,7 @@ my_email = email()
 my_email.set_default()
 my_email.set_to("whmowen@gmail.com")
 my_email.set_subject("Fetch torrent lists")
-my_email.set_content(torrent_html)
+my_email.set_content(torrent_response.text)
+
+
+# my_email.send()
