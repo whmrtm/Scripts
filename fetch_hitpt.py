02a672a362001e501cf023d20c67340d9f52e761
whmrtm <whmrtm@gmail.com>
2016-09-22 11:39:20 -0400

    else

--- fetch_hitpt.py
+++ fetch_hitpt.py
@@ -8,21 +8,20 @@ import json
 from my_email import email
 
 
-my_url = "https://pt.hit.edu.cn/login.php?returnto=torrents.php"
+my_url = "https://pt.hit.edu.cn/login.php"
 
 session_requests = requests.session()
 # result = session_requests.get(my_url)
 payload = json.dumps({'username':'whmrtn@126.com', 'password':'whm9316rtm'})
-result = session_requests.post(my_url, data = payload, headers = dict(referer = my_url)
-                )
-print(result.status_code)
-print(result.history)
-print(result.text)
+
+
+# Login
+result = session_requests.post(my_url, data = payload)
 
 torrent_url = "https://pt.hit.edu.cn/torrents.php"
 torrent_response = session_requests.get(torrent_url, headers = dict(referer = torrent_url), allow_redirects = True)
 
-
+print(torrent_response.text)
 # Send the email through my email class
 
 my_email = email()
