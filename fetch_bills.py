72173ce664ac5fbb983757e9a64e2884bb7476c2
whmrtm <whmrtm@gmail.com>
2016-09-24 16:13:32 -0400

    hydro

--- fetch_bills.py
+++ fetch_bills.py
@@ -1,21 +1,32 @@
 #!/bin/python
 from lxml import html
 from my_email import email
+import datetime
 import requests
 
-rogers_login_url = "https://www.rogers.com/web/totes/#signin"
+hydro_login = "https://www.wnhwebpresentment.com/app/capricorn?para=index"
 session = requests.session()
 placeholder = {
-                'USER':'whmrtm@gmail.com',
-                'password':'whm9316rtm'
+                'accessCode':'whmrtm@gmail.com',
+                'password':'WHM9316rtm',
+                'rememberMyAccountNumber':'Y'
                 }
-response = session.post(rogers_login_url, 
+response = session.post(hydro_login, 
                 data = placeholder,
-                headers = dict(referer = rogers_login_url))
+                headers = dict(referer = hydro_login))
+
+dat://www.wnhwebpresentment.com/app/capricorn?para=smartMeterConsum&inquiryType=hydro
+today = datetime.date.today()
+month = "%2d" % today.month
+to_day = "%2d" % today.day
+daily_consumption_url = "https://www.wnhwebpresentment.com/app/capricorn?para=smartMeterConsum&inquiryType=hydro&frommYear=2016&fromMonth=" + month +"&fromDay=01&toYear=2016&toMonth=" + month + "&toDay=" + to_day
+daily_response = session.get(daily_consumption_url, headers = dict(referer = daily_consumption_url))
+daily_html = daily_response.text
+
+#price_url = "https://www.wnhwebpresentment.com/app/capricorn?para=usageComparison&inquiryType=hydro"
+#print(daily_response.text)
+#price_response = session.get(price_url, headers = dict(referer = price_url))
+#price_html = price_response.text
 
 
-
-test = "https://www.rogers.com/web/totes/#/accountOverview"
-t = session.get(test, headers = dict(referer=test))
-print(t.history)
-print(t.status_code)
+print(daily_html + price_html)
