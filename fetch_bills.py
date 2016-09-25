5df99dc44dcc93f1972255265e8e14baee4a18e7
Heming Wang <whmrtm@gmail.com>
2016-09-24 20:47:47 -0400

    Update

--- fetch_bills.py
+++ fetch_bills.py
@@ -15,18 +15,17 @@ response = session.post(hydro_login,
                 data = placeholder,
                 headers = dict(referer = hydro_login))
 
-dat://www.wnhwebpresentment.com/app/capricorn?para=smartMeterConsum&inquiryType=hydro
 today = datetime.date.today()
-month = "%2d" % today.month
-to_day = "%2d" % today.day
-daily_consumption_url = "https://www.wnhwebpresentment.com/app/capricorn?para=smartMeterConsum&inquiryType=hydro&frommYear=2016&fromMonth=" + month +"&fromDay=01&toYear=2016&toMonth=" + month + "&toDay=" + to_day
+month = "%02d" % today.month
+to_day = "%02d" % today.day
+
+daily_consumption_url = "https://www.wnhwebpresentment.com/app/capricorn?para=smartMeterConsum"
 daily_response = session.get(daily_consumption_url, headers = dict(referer = daily_consumption_url))
 daily_html = daily_response.text
 
-#price_url = "https://www.wnhwebpresentment.com/app/capricorn?para=usageComparison&inquiryType=hydro"
-#print(daily_response.text)
-#price_response = session.get(price_url, headers = dict(referer = price_url))
-#price_html = price_response.text
+price_url = "https://www.wnhwebpresentment.com/app/capricorn?para=usageComparison&inquiryType=hydro&fromYear=2016&fromMonth="+ month +"&fromDay=01&toYear=2016&toMonth="+ month +"&toDay=" + to_day
+price_response = session.get(price_url, headers = dict(referer = price_url))
+price_html = price_response.text
 
 
 print(daily_html + price_html)
